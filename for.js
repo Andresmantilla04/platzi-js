7b2874817330d7343bc37cdae5f70d5f2f1b62e8
Andresmantilla04 <andresmantilla2004@gmail.com>
2023-06-04 13:09:12 -0500

    segundo commit

---
+++ for.js
@@ -0,0 +1,9 @@
+var estudiantes = ["Maria", "sergio", "rosa", "daniel"];
+
+function saludarEstudiantes(estudiante) {
+  console.log(`Hola, $ {estudiante}`);
+}
+
+for (var i = 0; i < estudiantes.length; i++) {
+  SaludarEstudiantes( `estudiantes `[i])
+} 
---
+++ objects.js
@@ -0,0 +1,13 @@
+//aqui se haria el listado de nuestro objeto con sus caracteristicas//
+var miAuto ={
+    marca: "Ferrari",
+    modelo: "Testarrosa",
+    annio: 1991
+};
+//aqui llamamos a nuestras caracteristicas//
+miAuto
+Object { marca: "Ferrari", modelo: "Testarrosa", annio: 1991 }
+//en caso de necesitar solo una caracteristica se busca como nos muestra el siguiente comando//
+miAuto.marca
+
+miAuto.annio
